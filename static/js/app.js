9f5d7a664ae5b2ac3f327829bf43897f0f3548d8
marky <42891574+MarkYocumII@users.noreply.github.com>
2019-04-03 20:37:14 -0500

    app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,4 @@
 // call our own API route
-// Since the data is already stored an array of traces, we 
-// simply pass the API's response into the plot function.
 
 var url = '/api/v1.0/drug_data';
 d3.json(url).then(function(data) {
