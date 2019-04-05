17734c2782c1c9a26faa7c5e0e0f3f4ec877b9aa
marky <42891574+MarkYocumII@users.noreply.github.com>
2019-04-04 21:39:29 -0500

    updated jupyter lab files with instructions on data mapping

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,12 @@
 // call our own API route
 
 var url = '/api/v1.0/drug_data';
-d3.json(url).then(function(data) {
-    console.log(data)
-  });
-
-var url = '/api/v1.0/unemployment';
-d3.json(url).then(function(data) {
-    console.log(data)
+d3.json(url).then(function (data) {
+  console.log(data.Drugs[0])
 });
+
+// var url = '/api/v1.0/unemployment';
+
+// d3.json(url).then(function (data) {
+//   console.log(data.Unemployment[0].Abbreviation == "DE")
+// });
