8f166552f8d14b64d5bb6d6db0c3bfd6ef9c06f0
Data Analyst 77002 <analyst77002@outlook.com>
2019-04-09 20:25:33 -0500

    pulling flask and pushing HTML

--- static/js/app.js
+++ static/js/app.js
@@ -5,8 +5,41 @@ d3.json(url).then(function (data) {
   console.log(data.Drugs[0])
 });
 
-// var url = '/api/v1.0/unemployment';
+var url = '/api/v1.0/unemployment';
 
-// d3.json(url).then(function (data) {
-//   console.log(data.Unemployment[0].Abbreviation == "DE")
-// });
+d3.json(url).then(function (data) {
+  console.log(data.Unemployment[0].Abbreviation == "DE")
+});
+
+chartGroup.call(toolTip);
+
+chartGroup.selectAll("rect")
+.data(dataArray)
+.enter()
+.append("rect")
+.attr("x", (d, i) => xScale(dataCategories[i]))
+.attr("y", d => yScale(d))
+.attr("width", xScale.bandwidth())
+.attr("height", d => chartHeight - yScale(d))
+.attr("fill", "green")
+// event listener for onclick event
+.on("click", function(d, i) {
+  alert(`Hey! You clicked bar ${dataCategories[i]}!`);
+})
+// event listener for mouseover
+.on("mouseover", function() {
+  d3.select(this)
+        .attr("fill", "red");
+})
+// event listener for mouseout
+.on("mouseout", function() {
+  d3.select(this)
+        .attr("fill", "green");
+});
+
+
+makeResponsive();
+
+// Event listener for window resize.
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,18 @@
-$("path, circle").hover(function(e) {
-    $('#info-box').css('display','block');
-    $('#info-box').html($(this).data('info'));
-  });
+// $("path, circle").hover(function(e) {
+//     $('#info-box').css('display','block');
+//     $('#info-box').html($(this).data('info'));
+//   });
   
-  $("path, circle").mouseleave(function(e) {
-    $('#info-box').css('display','none');
-  });
+//   $("path, circle").mouseleave(function(e) {
+//     $('#info-box').css('display','none');
+//   });
   
-  $(document).mousemove(function(e) {
-    $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-  }).mouseover();
-  
+  // $(document).mousemove(function(e) {
+  //   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+  //   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+  // }).mouseover();
+
+  function showID( elem)
+  {
+   alert ( 'ID is: ' + elem.id );
+  }
