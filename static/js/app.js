3a4c8ebf65573a0213ba40b656673a16770ff9ac
Data Analyst 77002 <analyst77002@outlook.com>
2019-04-10 08:26:49 -0500

    pulling States API

--- static/js/app.js
+++ static/js/app.js
@@ -4,45 +4,4 @@ var url = '/api/v1.0/combined';
 d3.json(url).then(function (data) {
   console.log(data)
 });
-<<<<<<< HEAD
 
-var url = '/api/v1.0/unemployment';
-
-d3.json(url).then(function (data) {
-  console.log(data.Unemployment[0].Abbreviation == "DE")
-});
-
-chartGroup.call(toolTip);
-
-chartGroup.selectAll("rect")
-.data(dataArray)
-.enter()
-.append("rect")
-.attr("x", (d, i) => xScale(dataCategories[i]))
-.attr("y", d => yScale(d))
-.attr("width", xScale.bandwidth())
-.attr("height", d => chartHeight - yScale(d))
-.attr("fill", "green")
-// event listener for onclick event
-.on("click", function(d, i) {
-  alert(`Hey! You clicked bar ${dataCategories[i]}!`);
-})
-// event listener for mouseover
-.on("mouseover", function() {
-  d3.select(this)
-        .attr("fill", "red");
-})
-// event listener for mouseout
-.on("mouseout", function() {
-  d3.select(this)
-        .attr("fill", "green");
-});
-
-
-makeResponsive();
-
-// Event listener for window resize.
-// When the browser window is resized, makeResponsive() is called.
-d3.select(window).on("resize", makeResponsive);
-=======
->>>>>>> ffc65865a287d2918b9995a0e5d69f7d8494eba2
